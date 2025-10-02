7c11879220e1098101b4c502b0d1948fafc188b6
Joel Vigário <102935482+JoelVigario@users.noreply.github.com>
2025-10-02 16:19:04 +0100

    Desafio 001 - Olá Mundo

---
+++ d001_OlaMundo/src/app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const app = express()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '../public')))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'))
+})
+
+export default app
